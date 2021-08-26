6ffb9ce4c2705dcba20207bc313eaddfa3e27344
Ian Holm <ianholm252@gmail.com>
2021-08-26 14:18:57 -0600

    trying to make css work

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -17,7 +17,7 @@ app.get('/js', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../style.css'))
 //     })
 
-  app.use('/css', express.static(path.join(__dirname, 'style.css')))
+  app.use('/css', express.static(path.join(__dirname, '/style.css')))
 
 app.listen(port, () => {
     console.log(`Running on ${port}`)
