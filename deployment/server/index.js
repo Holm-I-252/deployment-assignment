6e634de0a762246e765592712a891c93bcc5dc40
Ian Holm <ianholm252@gmail.com>
2021-08-26 14:07:44 -0600

    trying to make css work

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -13,6 +13,10 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../main.js'))
   })
 
+app.get('/jfif', (req, res) => {
+    res.sendFile(path.join(__dirname, '../hackerman.jfif'))
+})
+
 // app.get('/css', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../style.css'))
 //     })
